30599356c30f31d9c75f1de197267c007c1b650d
ymdryy <110339325+ymdryy@users.noreply.github.com>
2022-12-09 21:11:30 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ fetch("quiz.json")
 //カウントダウン
 let countdown = setInterval(function () {
     const now = new Date()
-    const target = new Date("2022/12/8 10:40:00") //ターゲット日時を取得
+    const target = new Date("2023/1/1 0:00:00") //ターゲット日時を取得
     const remainTime = target - now
 
     if (remainTime < 0) return false
